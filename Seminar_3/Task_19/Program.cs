7f6219503ebaa70871f177f2d1c55deca15e7665
Magomed <abdulaev_21@bk.ru>
2022-12-08 23:29:46 +0300

    домашнее задание (семинар 3)

---
+++ Seminar_3/Task_19/Program.cs
@@ -0,0 +1,23 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Write("Введите пятизначное число: ");
+string number = Console.ReadLine()!;
+int lenght = number.Length;
+if (lenght == 5)
+{
+    if (number[0] == number[4] && number[1] == number[3])
+    {
+        Console.Write($"{number} - палиндром");
+    }
+    else
+    {
+        Console.Write($"{number} - НЕ палиндром");
+    }
+}
+else
+{
+    Console.Write("Введено недопустимое значение");
+}
---
+++ Seminar_3/Task_21/Program.cs
@@ -0,0 +1,17 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.Write("Введите X координату первой точки: ");
+int X1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y координату первой точки: ");
+int Y1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Z координату первой точки: ");
+int Z1 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите X координату второй точки: ");
+int X2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Y координату второй точки: ");
+int Y2 = int.Parse(Console.ReadLine()!);
+Console.Write("Введите Z координату второй точки: ");
+int Z2 = int.Parse(Console.ReadLine()!);
+Console.Write(Math.Sqrt(Math.Pow(X1 - X2, 2) + Math.Pow(Y1 - Y2, 2) + Math.Pow(Z1 - Z2, 2)));
