a0d30310a26f4db12ac98ccca80fae0532f6b8e3
Magomed <abdulaev_21@bk.ru>
2022-12-14 20:45:40 +0300

    домашнее задание (семинар 4)

---
+++ Seminar_4/Task_27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+Console.Write("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+Console.Write(SumOfNumbers(number));
+int SumOfNumbers(int a)
+{
+    int sum = 0;
+    while(a > 0)
+    {
+        sum = sum + a % 10;
+        a = a / 10;
+    }
+    return sum;
+}
---
+++ Seminar_4/Task_29/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход числа n, min, max. Затем генерирует массив размером n и заполняет его случайными числами в диапазоне от min до max
+// 5, 1, 19 -> [1, 2, 5, 7, 19]
+// 3, 1, 33 -> [6, 1, 33]
+
+Console.Write("Введите число N: ");
+int a = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число MIN: ");
+int b = int.Parse(Console.ReadLine()!);
+Console.Write("Введите число MAX: ");
+int c = int.Parse(Console.ReadLine()!);
+int[] array = GenerateArray(a, b, c);
+for (int i = 0; i < array.Length; i++)
+{
+    Console.Write(array[i] + " ");
+}
+int[] GenerateArray(int n, int min, int max)
+{
+    int[] array = new int[n];
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+    return array;
+}
