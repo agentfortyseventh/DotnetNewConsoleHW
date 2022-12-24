88a0466a8f3c82cba38bb3744a1091e5d5f4d0e9
Magomed <abdulaev_21@bk.ru>
2022-12-24 23:12:49 +0300

    домашнее задание (семинар 7)

---
+++ Seminar_7/Task_47/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами
+// m = 3, n = 4
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+Console.Write("Кол-во строк: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Кол-во столбцов: ");
+int n = int.Parse(Console.ReadLine()!);
+double[,] matrix = Create2DArray(m, n, -10, 10);
+Print2DArray(matrix);
+double[,] Create2DArray(int m, int n, int minValue, int maxValue)
+{
+    double[,] matrix = new double[m, n];
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = Math.Round((maxValue - minValue) * new Random().NextDouble() + minValue, 2);
+        }
+    }
+    return matrix;
+}
+void Print2DArray(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
---
+++ Seminar_7/Task_50/Program.cs
@@ -0,0 +1,44 @@
+﻿// Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 1
+// 7 -> такого числа в массиве нет
+
+Console.Write("Индекс строки: ");
+int m = int.Parse(Console.ReadLine()!);
+Console.Write("Индекс столбца: ");
+int n = int.Parse(Console.ReadLine()!);
+int[,] matrix = new int[7, 7];
+Create2DArray(matrix);
+Print2DArray(matrix);
+if (m < matrix.GetLength(0) && n < matrix.GetLength(1))
+{
+    Console.Write($"по этому адресу число - {matrix[m, n]}");
+}
+else
+{
+    Console.Write($"{m}, {n} - такого адреса не существует");
+}
+void Create2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+void Print2DArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
